742d10aa0954deb9befc53ec03fc7ea39c2f2a97
Sleepy0521 <31335878+Sleepy0521@users.noreply.github.com>
2019-04-16 18:20:59 +0800

    filesever成功启动  接下来改造provider

--- music-fileserver-8020/src/main/java/com/music/FileServer8020.java
+++ music-fileserver-8020/src/main/java/com/music/FileServer8020.java
@@ -1,6 +1,5 @@
 package com.music;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -12,10 +11,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @EnableDiscoveryClient //服务发现
 @EnableCircuitBreaker//对hystrixR熔断机制的支持
-public class FileServer8000 extends SpringBootServletInitializer {
+public class FileServer8020 extends SpringBootServletInitializer {
 
       public static void main(String[] args) {
-          SpringApplication.run(FileServer8000.class, args);
+          SpringApplication.run(FileServer8020.class, args);
       }
 
 }
